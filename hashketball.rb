bf5411a38d179468e828317f6a20641f44364d01
Adam Altmark <adamaltmark@gmail.com>
2020-05-19 17:15:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,11 +191,11 @@ end
 end
 
 def big_shoe_rebounds
+  max_shoe
   game_hash.each do |team, team_info|
     max_shoe = team_info[:players].max_by {|player_hash|player_hash[:shoe_size]}
       #player_hash.max_by {players| player_hash[:shoe_size]}
     end
-    max_shoe
     binding.pry
 end
 
